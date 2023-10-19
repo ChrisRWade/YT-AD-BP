44c5b4a3a73e39a534f16da1765e87cde339ff2c
Chris Wade <crwade11@gmail.com>
2023-10-18 21:09:12 -0400

    new bypass for recent hotfix

--- content.js
+++ content.js
@@ -2,26 +2,44 @@ console.log("Content script loaded");
 
 let observer;
 
+setInterval(() => {
+  console.log("Checking for popups");
+  const popupContainer = document.querySelector("ytd-popup-container");
+  if (popupContainer) {
+    console.log("Popup found, removing");
+    popupContainer.remove();
+  }
+}, 1000); // Checks every 1 second
+
 function startObserver() {
   console.log("Starting observer");
   const config = {childList: true, subtree: true};
+
+  const urlObserverConfig = {
+    childList: true,
+    subtree: true,
+    characterData: true,
+  };
+  const urlObserverCallback = function () {
+    const popupContainer = document.querySelector("ytd-popup-container");
+    if (popupContainer) {
+      popupContainer.remove();
+    }
+  };
+  const urlObserver = new MutationObserver(urlObserverCallback);
+  urlObserver.observe(document.body, urlObserverConfig);
 
   const callback = function (mutationsList, observer) {
     for (let mutation of mutationsList) {
       if (mutation.addedNodes.length) {
         mutation.addedNodes.forEach((node) => {
-          if (
-            node.nodeName.toLowerCase() === "ytd-enforcement-message-view-model"
-          ) {
-            const closeButton = document.querySelector(
-              "yt-button-view-model#dismiss-button button"
+          if (node.nodeName.toLowerCase() === "ytd-popup-container") {
+            const popupContainer = document.querySelector(
+              "ytd-popup-container"
             );
-            if (closeButton) closeButton.click();
-
-            setTimeout(() => {
-              const playButton = document.querySelector(".ytp-play-button");
-              if (playButton) playButton.click();
-            }, 1000);
+            if (popupContainer) {
+              popupContainer.remove();
+            }
           }
         });
       }
@@ -52,9 +70,4 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 chrome.runtime.sendMessage({status: "ready"});
 
 // Re-start observing on page navigation
-window.addEventListener("popstate", startObserver);
-// Non-standard events; may not work as expected
-// window.addEventListener("pushState", startObserver);
-// window.addEventListener("replaceState", startObserver);
-// Alternative: custom event fired by some libraries
-// window.addEventListener("statechange", startObserver);
+window.addEventListener("hashchange", startObserver);
